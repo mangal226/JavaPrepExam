7bb470964450cae1d29e3d50355eea1f5aa75764
Hamed LY <hamed.ly@soprasteria.com>
2024-05-04 15:08:05 +0200

    Continuation with operators and variables

--- src/datatypes/operators/OperatorsAndMathAPIs.java
+++ src/datatypes/operators/OperatorsAndMathAPIs.java
@@ -22,10 +22,39 @@ public class OperatorsAndMathAPIs {
 
         int numberOne = 2;
 
-        int numberTwo = 2;
+        int numberTwo = 5;
+
+        /**
+         * Difference between && and & : && don't take time to check the other side if the first side
+         * permit to give the answer i.e a>b && a>c if a<b it return false don't need to
+         * check the second operation. It is more faster and avoid wasting time
+         * The same for | and ||
+         */
+        Boolean isOrOK = numberOne >= 2 || numberTwo >= 5;
+
+        Boolean isAndOK = numberOne >= 2 && numberTwo > 5;
+
+        Boolean isOrxOK = numberOne>=2 ^ numberTwo>5 ? true: false;
+
+        Boolean isNotOK = !(numberOne>2 && numberTwo>5) ? true: false;
+
 
         //Does not exist in java
         // Boolean equalsNumberTwo = ( numberOne === numberTwo);
+        /**
+         * Other operators
+         * &=, ^=, |=, <<=, >>= , >>>=
+         */
+
+        /**
+         * Assignment operators
+         * *=, /=, %=
+         */
+
+        /**
+         * Relationnal operators
+         * instanceof, <, >, <=, >=
+         */
 
         /**
          * Custom wrappers
@@ -49,6 +78,14 @@ public class OperatorsAndMathAPIs {
         System.out.println("Verify if integer : " + isInteger);
 
         System.out.println("Does two numbers equals even if there a different types : " + equalsNumberOne);
+
+        System.out.println("IsOrOk : " + isOrOK);
+
+        System.out.println("IsAndOK : " + isAndOK);
+
+        System.out.println("IsOrxOK : " + isOrxOK);
+
+        System.out.println("IsNotOK : " + isNotOK);
 
 
     }
--- src/datatypes/primitives/PrimitiveTypesAndVariables.java
+++ src/datatypes/primitives/PrimitiveTypesAndVariables.java
@@ -45,6 +45,28 @@ public class PrimitiveTypesAndVariables {
         int int1 = 134;
         int int2 = 340;
 
+        /**
+         * Scientific notation
+         */
+        double lightSpeed = 5.88e12;
+        float diameterHydr = 5.0e-8f;
+
+
+        /**
+         * Narrowing widenning and casting
+         * PS: When narrowing it seems that the variable is casted
+         */
+        int myIntNumber = 56;
+        short myShortFromInt = (short) myIntNumber;
+        byte myByteFromShort = (byte) myShortFromInt;
+        long myLongFromInt = myIntNumber;
+        long myLongTwo = 456778;
+        int myIntFromLong = (int) myLongTwo;
+        long myLongValue = 9223372036854775807L;
+        int myIntValueOverflow = (int) myLongValue;
+        short myShortValueOverflow = (short) myIntValueOverflow;
+        byte myByteValueOverflow = (byte) myShortValueOverflow;
+
 
         Date myDate = new Date();
         LocalDateTime myLocalDate = LocalDateTime.now();
@@ -86,6 +108,22 @@ public class PrimitiveTypesAndVariables {
         System.out.println("Printing the date : " + myDate);
 
         System.out.println("Date formated : " + dateFormated);
+
+        System.out.println("Short from int : " + myShortFromInt);
+
+        System.out.println("Byte from short : " + myByteFromShort);
+
+        System.out.println("Int from long : " + myIntFromLong);
+
+        System.out.println("Long from int : " + myLongFromInt);
+
+        System.out.println("Int overflow value from long : " + myIntValueOverflow);
+
+        System.out.println("Short overflow value from int : " + myShortValueOverflow);
+
+        System.out.println("The light speed is : " + lightSpeed);
+
+        System.out.println("The diameter of hydrogen is : " + diameterHydr);
 
     }
 }
