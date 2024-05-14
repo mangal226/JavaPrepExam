1f851321e0632100aabef3bfc4dadd96771ca82b
Hamed LY <mangali226@yahoo.fr>
2024-05-14 13:35:12 +0200

    JavaPrepExam day 5

--- src/programFlow/Looping.java
+++ src/programFlow/Looping.java
@@ -8,7 +8,28 @@ public class Looping {
         whileLoopDays();
         whileLoopFactorial(10);
         forLoop();
+        factorialWithForLoop(5);
+        /**
+         * Limitations of foreach
+         * we can't work with two arrays together
+         */
 
+        int[] array1 = {1, 2, 3, 4};
+        int[] array2 = {5, 6, 7, 8};
+        sumOfArraysWithForLoop(array1,array2);
+        nestingForLoop(array1);
+        nestingForLoop(array2);
+
+        int[][] doubleArray = {{1, 2}, {2, 3}, {4, 5}, {6, 7}, {8, 9}};
+        nestedForLoop(doubleArray);
+
+        int[][][] tripleArray = new int[2][8][11];
+        System.out.println(tripleArray.length);
+
+        loopPowerNotEvenContinue(10);
+        System.out.println("Before function call");
+        loopPowerNotEvenReturn(100);
+        System.out.println("After function call");
     }
 
     private static void whileLoopDays(){
@@ -48,7 +69,87 @@ public class Looping {
                 .forEach(letter -> System.out.println("Printing the letter with stream : " + letter));
 
         for(letters letter : table){
-            System.out.println("Printing letter with element in for : " + letter);
+            System.out.println("Printing letter with foreach : " + letter);
         }
+    }
+
+    private static void factorialWithForLoop(int number){
+        int factorial = 1;
+        if(number> 1) for(int i= 1; i<= number; i++) factorial = factorial*i;
+        else if (number == 0 ) factorial =1;
+        else if (number == 1) factorial = 1;
+        else factorial = -1;
+
+        System.out.println(factorial);
+    }
+
+    private static void sumOfArraysWithForLoop(int [] arrayOne, int[] arrayTwo){
+        int[] sumReverseArray = new int[arrayTwo.length];
+        for(int i=0, j= arrayTwo.length-1; i< arrayTwo.length; i++,j--)
+            sumReverseArray[i]=arrayOne[i] + arrayTwo[j];
+
+        System.out.println("The sum of reverse array is : ");
+        for(int sum : sumReverseArray)
+                System.out.println("The sum for this index is : " + sum);
+
+    }
+
+    /**
+     * Nesting ps: using if-else in loop or vice-versa
+     */
+    private static void nestingForLoop(int[] array){
+        int sumOfEven = 0;
+        int sumOfOdd = 0;
+        for(int number : array)
+            if(number%2 == 0)
+                sumOfEven += number;
+            else sumOfOdd += number;
+
+        System.out.println("Sum of even numbers : " + sumOfEven);
+        System.out.println("Sum of odd numbers : " + sumOfOdd);
+    }
+
+    private static void nestedForLoop(int[][] array){
+        System.out.println(array.length);
+        for(int i=0; i< array.length; i++){
+            for (int j=0; j<array[i].length; j++)
+                System.out.println("We are at the line : " +i+ ", column : " +j);
+        }
+    }
+
+    private static void loopPowerNotEvenContinue(int number){
+        int sumOfPow = 1;
+        int i =0;
+        while (i<number){
+            i++;
+            System.out.println("The number is : " + i);
+            if(i%2==0) sumOfPow += i*i;
+            else continue;
+            System.out.println("The sum for " + i + " is : " + sumOfPow);
+            if(sumOfPow > 50) break;
+
+        }
+        System.out.println("The sum of power is : " + sumOfPow);
+    }
+
+    /**
+     *
+     * Difference between return and break:
+     * return exit the function
+     * break quit the looping process and continue the function
+     */
+    private static void loopPowerNotEvenReturn(int number){
+        int sumOfPow = 1;
+        int i =0;
+        while (i<number){
+            i++;
+            System.out.println("The number is : " + i);
+            if(i%2==0) sumOfPow += i*i;
+            else continue;
+            System.out.println("The sum for " + i + " is : " + sumOfPow);
+            if(sumOfPow > 100) return;
+
+        }
+        System.out.println("The sum of power is : " + sumOfPow);
     }
 }
