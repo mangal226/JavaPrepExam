5fa3bcab0e164729d0e31d5abc556551964d507c
Hamed LY <mangali226@yahoo.fr>
2024-09-05 07:21:45 +0200

    JavaPrepExam day 15: Java Module System
    --> Creating and running a module without an IDE

--- src/module-info.java/src/Main.java
+++ src/module-info.java/src/Main.java
@@ -8,6 +8,28 @@ public class Main {
      * --> Avoid terminal digit like myFirstModule2 ...
      */
 
+    /**
+     *
+     * Compiling modules
+     * --> javac -d out src/com/javamodularity/greeter/Main.java module-info.java testi.java i.e. compile the listed java files
+     * --> javac -d out --module-source-path src -m greeter i.e. compiles the java files in the directory src
+     *
+     * Running java code
+     * --> java -p out -m src/com.javamodularity.greeter.Main
+     */
+    /**
+     * Exporting packages from module
+     * In the module declaration we can add the tag exports e.g.
+     * module mymodule{
+     *     exports com.pluralsight;
+     * }
+     */
+
+    /**
+     *
+     * @param args
+     */
+
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/moduleOutside/creating_module1/src/com/javamodularity/greeter/Main.java
@@ -0,0 +1,7 @@
+package com.javamodularity.greeter;
+
+public class Main {
+  public static void main(String... args) {
+    System.out.println("Greetings from a module!");
+  }
+}
