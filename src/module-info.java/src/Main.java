ee21392905197764c73d7d96fa777791ce0e8cc1
Hamed LY <mangali226@yahoo.fr>
2024-09-08 06:51:21 +0200

    JavaPrepExam day 16: Java Module System
    --> Using services
    --> Running different modules

--- src/module-info.java/src/Main.java
+++ src/module-info.java/src/Main.java
@@ -46,6 +46,38 @@ public class Main {
      * open module mymodule{}
      * what is the difference between open and export for modules
      * open is used only for the run time but export is used for both compile and run time
+     *
+     * Before the modular JDK there was only one jar called rt. i.e. rt = runtime
+     * java --list-modules permits to list all the modules included in the JDK
+     *
+     * Why does java.sql module requires java.logging module?
+     * package java.sql
+     * import java.util.logging.Logger
+     * public interface Driver{
+     *     public Logger getParentLogger();
+     * }
+     * Because the java.util needed by java.sql requires java.logging
+     * What does require transitive means?
+     * mymodule requires java.sql
+     * java.sql requires transitive java.logging
+     * --> this means mymodule requires java.logging i.e. a transitive require has been applicated
+     *
+     * Packaging a module
+     * jar --create --file=mods/pluralsight.jar -C out/ .
+     *
+     * Decoupling with Services
+     * Supposed that we have two
+     *
+     * In compiling multiple modules and listing them mind not to add space after the commas when listing
+     * e.g. javac -d out --module-source-path src -m greeter.cli,greeter.hello,greeter.friendly
+     * when you add , and space it won't compile
+     *
+     * How to run multiple modules in java:
+     * $ java --module-path "out;mods/greeter.grumpy.jar" -m greeter.cli/greeter.cli.Main
+     * Hello from a module!
+     * Grumpy greetings from a module!
+     * Friendly greetings from a module!
+     * It runs the modules in out and the module in greeter.grumpy.jar
      */
 
     /**
